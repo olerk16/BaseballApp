aebe0fa852eb7b3a3cf6d15f004408380be318db
olerk16 <netlocolerk16@hotmail.com>
2024-11-09 13:18:50 -0600

    add pitch health metrics

--- schema.js
+++ schema.js
@@ -1,22 +1,29 @@
 const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
 
-const PlayerMetricType = new GraphQLObjectType({
-  name: 'PlayerMetric',
-  fields: {
-    playerId: { type: GraphQLString },
-    score: { type: GraphQLInt },
-  },
-});
+const PitchDataType = new GraphQLObjectType({
+    name: 'PitchData',
+    fields: {
+      pitchId: { type: GraphQLString },
+      playerId: { type: GraphQLString },
+      pitchCount: { type: GraphQLInt },
+      speed: { type: GraphQLInt },
+      pitchType: { type: GraphQLString }, // fastball, curveball, ect...
+      pitchMet: { type: GraphQLBoolean },
+      targetLocation: { type: GraphQLString }, // top-right, top-center, top-left, center-left, center-right center low-center, low-right, low-left
+      dateTime: { type: GraphQLString }
+    },
+  });
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    playerMetric: {
-      type: PlayerMetricType,
+    pitchData: {
+      type: PitchDataType,
       args: { playerId: { type: GraphQLString } },
       resolve(parent, args) {
-        // Retrieve data from DynamoDB or Redis here
-        return { playerId: args.playerId, score: 42 }; // Mock data
+        // Retrieve data from DynamoDB or Redis
+        return { playerId: args.playerId, pitchCount: 42, pitchMet: true }; // Mock data
+      },
       },
     },
   },
